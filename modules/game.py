9d0859dadb6a5be35c25c602092e745cbb500ef2
marklivolsi <mark.c.livolsi@gmail.com>
2019-02-21 22:36:38 -0500

    Add Game print_table() class method

--- modules/game.py
+++ modules/game.py
@@ -10,3 +10,12 @@ class Game:
         self.quick_game = quick_game
         self.dealer = Player(name='Dealer')
         self.player_list = []
+
+    def print_table(self, show_dealer=False):
+        if show_dealer:
+            print("Dealer's hand: {} ({} points)".format(self.dealer.hands[0], self.dealer.hands[0].points))
+        else:
+            print("Dealer's hand: [{}, ???]".format(self.dealer.hands[0][0]))
+        for player in self.player_list:
+            for hand in player.hands:
+                print(hand_detail(player, hand))
