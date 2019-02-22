01f50af13cec3c67f20e91a627f97687aa9c7234
marklivolsi <mark.c.livolsi@gmail.com>
2019-02-21 22:43:00 -0500

    Add Game class method set_game_type()

--- modules/game.py
+++ modules/game.py
@@ -11,6 +11,14 @@ class Game:
         self.dealer = Player(name='Dealer')
         self.player_list = []
 
+    def set_game_type(self):
+        print("Would you like to play a quick game? Quick games are one player versus the dealer,\n"
+              "and the player starts off with $1,000 in chips. Normal games allow you to set the number of players,\n"
+              "player names, chip amounts for each player, and number of decks in the dealer's shoe.\n")
+        quick_game = yes_no_input("Type 'yes' for a quick game, or 'no' for a normal game: \n")
+        if not quick_game:
+            self.quick_game = False
+
     def print_table(self, show_dealer=False):
         if show_dealer:
             print("Dealer's hand: {} ({} points)".format(self.dealer.hands[0], self.dealer.hands[0].points))
